b6341004184b66aca8f636154e4e504ada88ad5a
sist115_1 <sist115_1@sist115>
2024-04-12 13:41:10 +0900

    1

---
+++ Chapter01_자바설치/src/MainClass.java
@@ -0,0 +1,15 @@
+/*
+ * 	여러줄 주석
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println('A');
+		System.out.println((int)'A');
+		System.out.println(Integer.toBinaryString(65));
+		System.out.println((int)'홍');
+		System.out.println(Integer.toBinaryString(54861));
+	}
+
+}
