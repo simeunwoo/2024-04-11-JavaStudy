6e339c2fca78c88924967ec0873419d13c32bd18
sist115_1 <sist115_1@sist115>
2024-04-16 12:50:48 +0900

    17

---
+++ Chapter03_제어문_1/src/제어문_단일조건문_3.java
@@ -0,0 +1,12 @@
+/*
+ * 	if (조건문) => 웹 (page를 나눈 경우, 상세보기, 검색)
+ * 		=> 사람 => 한 눈에 볼 수 있는 이미지 => 15~20개 정도
+ */
+public class 제어문_단일조건문_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
