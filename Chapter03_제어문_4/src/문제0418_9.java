d90a176acf70c03175352310c00a7562429c35a9
sist115_1 <sist115_1@sist115>
2024-04-18 17:51:29 +0900

    0418

--- Chapter03_제어문_4/src/문제0418_9.java
+++ Chapter03_제어문_4/src/문제0418_9.java
@@ -3,10 +3,12 @@ public class 문제0418_9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int sum=0;
 		for(int a=1;a<=10;a++)
 		{
-			if(a%3==0)
+			if(a%3!=0)
+			{
+				System.out.print(a+" ");
+			}
 			
 		}
 			
