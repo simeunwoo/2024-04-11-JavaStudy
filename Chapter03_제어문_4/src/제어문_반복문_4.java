2088b67a2be797c07b0e285b36642770989b1841
sist115_1 <sist115_1@sist115>
2024-04-18 14:27:23 +0900

    17

--- Chapter03_제어문_4/src/제어문_반복문_4.java
+++ Chapter03_제어문_4/src/제어문_반복문_4.java
@@ -16,12 +16,37 @@
  *
  *	*** 무한루프 : 계속 반복
  *	for(;;) => 무한루프형식 (무한 스크롤) => 자바에서 (X) => 자바스크립트
+ *												=> 타입스크립트
+ *												=> 우대사항
+ *	반복문을 제어
+ *	=> continue : 특정부분 제외
+ *	   break : 반복문 중단
+ *
+ *	list-wrap
  */
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 public class 제어문_반복문_4 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		int k=1;
+		for(int i=1;i<=4;i++)
+		{
+			Document doc=Jsoup.connect("https://www.genie.co.kr/chart/top200?ditc=D&ymd=20240418&hh=14&rtm=Y&pg"+i).get();
+			Elements title=doc.select("table.list-wrap a.title");
+			Elements singer=doc.select("table.list-wrap a.artist");
+			for(int j=0;j<title.size();j++)
+			{
+				System.out.println(k++);
+				System.out.println(title.get(j).text());
+				System.out.println(singer.get(j).text());
+			}
+		}
 	}
 
 }
