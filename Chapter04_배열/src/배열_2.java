1dc456a7f7cc956f1ad09d118b0d84db347989ee
sist115_1 <sist115_1@sist115>
2024-04-24 11:11:37 +0900

    0424

--- Chapter04_배열/src/배열_2.java
+++ Chapter04_배열/src/배열_2.java
@@ -95,7 +95,46 @@ public class 배열_2 {
 		 *		arr[0]=100;
 		 *		arr[1]=200;
 		 *		arr[2]=300;
+		 *
+		 *		for-each
+		 *		--------
+		 *		int[] arr
+		 *		double[] arr
+		 *		char[] arr
+		 *		for(데이터형 변수:배열명)
+		 *			---------
+		 *			int
+		 *			double
+		 *			char
+		 *		배열에 저장된 실제 데이터값을 읽어온다
+		 *		~~~~~~~~~~~~~~~~~~~ 배열의 모든 값을 읽으면 자동 종료
+		 *		예)
+		 *			int[] arr={10,20,30,40,50}
+		 *			----- 데이터형 일치 => 배열명
+		 *			for(int i:arr)
+		 *				i=10
+		 *				i=20
+		 *				i=30
+		 *				i=40
+		 *				i=50 => for 종료
+		 *
+		 *			char[] arr={'A','B','C'}
+		 *			for(char c:arr)
+		 *				c='A'
+		 *				c='B'
+		 *				c='C' => for 종료
+		 *
+		 *		=====> 사용 => 출력과 관련 (브라우저에서 목록)
+		 *
+		 *		char[] arr={'A','B','C'}
+		 * 		for(int i:arr)
+		 * 				=====> 가능! (char->int는 작->큰이므로 가능)
+		 * 		for((char)int i:arr) => 불가능! (형변환 불가)
+		 * 		=> byte < char < int < long < float < double
+		 * 		=> 형변환은 사용 불가능
 		 * 
+		 * 		(double)int a; => 불가능
+		 * 		int a=(int)10.5; => 가능
 		 */
 	}
 
