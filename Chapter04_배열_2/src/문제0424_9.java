936c1330423cce070f9ff45c57b5e59a60c8532a
sist115_1 <sist115_1@sist115>
2024-04-24 16:34:45 +0900

    0424

---
+++ Chapter04_배열_2/src/문제0424_9.java
@@ -0,0 +1,12 @@
+
+public class 문제0424_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {10,20,30,50,3,60,-3};
+		for(int i=0;i<arr.length;i++)
+		if(arr[i]==60)
+			System.out.println(i);
+	}
+
+}
