3f344dffcfbff7b29972bb2c9f5a6655a010a738
sist115_1 <sist115_1@sist115>
2024-04-24 10:52:30 +0900

    0424

--- Chapter04_배열/src/배열_1.java
+++ Chapter04_배열/src/배열_1.java
@@ -66,11 +66,112 @@
  *			=> double[] arr=new double[갯수];
  *			=> 배열 데이터형 배열명 = new 저장 갯수
  */
+// 84page => 배열 필요성
+// 3명의 학생 => 국어, 영어, 수학 => 1. 총점, 2. 평균, 3. 학점
+import java.util.Scanner;
 public class 배열_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan=new Scanner(System.in);
+		int kor1, kor2, kor3;
+		int eng1, eng2, eng3;
+		int math1, math2, math3;
+		int total1, total2, total3;
+		double avg1, avg2, avg3;
+		char score1, score2, score3;
+		
+		// 점수 입력 => 낱개로 적으면 점수를 9번 적어야 되서 비효율적임
+		System.out.print("첫째 학생의 국어 점수:");
+		kor1=scan.nextInt();
+		System.out.print("첫째 학생의 영어 점수:");
+		eng1=scan.nextInt();
+		System.out.print("첫째 학생의 수학 점수:");
+		math1=scan.nextInt();
+		System.out.print("둘째 학생의 국어 점수:");
+		kor2=scan.nextInt();
+		System.out.print("둘째 학생의 영어 점수:");
+		eng2=scan.nextInt();
+		System.out.print("둘째 학생의 수학 점수:");
+		math2=scan.nextInt();
+		System.out.print("셋째 학생의 국어 점수:");
+		kor3=scan.nextInt();
+		System.out.print("셋째 학생의 영어 점수:");
+		eng3=scan.nextInt();
+		System.out.print("셋째 학생의 수학 점수:");
+		math3=scan.nextInt();
+		// 총점
+		total1=kor1+eng1+math1;
+		total2=kor2+eng2+math2;
+		total3=kor3+eng3+math3;
+		// 평균
+		avg1=total1/3.0;
+		avg2=total2/3.0;
+		avg3=total3/3.0;
+		// 학점
+		switch(total1/30)
+		{
+		case 10: case 9:
+			score1='A';
+			break;
+		case 8:
+			score1='B';
+			break;
+		case 7:
+			score1='C';
+			break;
+		case 6:
+			score1='D';
+			break;
+		default:
+			score1='F';
+		}
+		switch(total2/30)
+		{
+		case 10: case 9:
+			score2='A';
+			break;
+		case 8:
+			score2='B';
+			break;
+		case 7:
+			score2='C';
+			break;
+		case 6:
+			score2='D';
+			break;
+		default:
+			score2='F';
+		}
+		switch(total3/30)
+		{
+		case 10: case 9:
+			score3='A';
+			break;
+		case 8:
+			score3='B';
+			break;
+		case 7:
+			score3='C';
+			break;
+		case 6:
+			score3='D';
+			break;
+		default:
+			score3='F';
+		}
+		// 결과값 출력
+		/*
+		 * 	%-5d : 00000 (뒤에서부터)(5칸) / 90 => 90---90---90---...
+		 * 	%5d : 00000 (앞에서부터)(5칸) / 90 => ---90---90---90...
+		 * 		=> 간격 조절 역할
+		 */
+		System.out.printf("%-5d%-5d%-5d%-7d%-7.2f%-3c\n",
+				kor1,eng1,math1,total1,avg1,score1); // 92   87   99   278    92.67  A  
+		System.out.printf("%-5d%-5d%-5d%-7d%-7.2f%-3c\n",
+				kor2,eng2,math2,total2,avg2,score2); // 95   79   58   232    77.33  C  
+		System.out.printf("%-5d%-5d%-5d%-7d%-7.2f%-3c\n",
+				kor3,eng3,math3,total3,avg3,score3); // 89   88   60   237    79.00  C  
 	}
 
 }
