5d079ec3139cef2b08c528cb986473df6e1c4ff1
sist115_1 <sist115_1@sist115>
2024-04-25 17:26:58 +0900

    0425

--- Chapter04_문자열배열_2/src/문제0425_2.java
+++ Chapter04_문자열배열_2/src/문제0425_2.java
@@ -4,11 +4,16 @@ public class 문제0425_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] person= {50,45,35,12,70};
-		System.out.println("사원(1) "+person[0]);
+/*		System.out.println("사원(1) "+person[0]);
 		System.out.println("사원(2) "+person[1]);
 		System.out.println("사원(3) "+person[2]);
 		System.out.println("사원(4) "+person[3]);
-		System.out.println("사원(5) "+person[4]);
+		System.out.println("사원(5) "+person[4]); */
+		
+		for(int i=0;i<person.length;i++)
+		{
+			System.out.println("사원("+(i+1)+") "+person[i]);
+		}
 		
 		int sum=0;
 		int min=100;
