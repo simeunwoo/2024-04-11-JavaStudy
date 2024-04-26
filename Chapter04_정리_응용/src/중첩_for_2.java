6510db1c7780398a424b67bd6f83537fd2dfd284
sist115_1 <sist115_1@sist115>
2024-04-26 09:27:20 +0900

    0426

---
+++ Chapter04_정리_응용/src/중첩_for_2.java
@@ -0,0 +1,28 @@
+/*
+ * 	구구단
+ * 	
+ * 	줄수 => 1차 for => 9번 수행 (1~9)
+ * 	2*1=2 ............... 9*1=9 => 첫째줄 8번 수행 (2~9)
+ * 	...
+ * 	...
+ * 	...
+ * 	2*9=18 .............. 9*9=81
+ */
+public class 중첩_for_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=9;i++)
+		{
+			for(int j=2;j<=9;j++)
+			{
+				// 구구단 출력
+					System.out.printf("%2d * %2d = %2d\t",j,i,j*i);
+			}
+			// 줄수
+			System.out.println();
+		}
+
+	}
+
+}
