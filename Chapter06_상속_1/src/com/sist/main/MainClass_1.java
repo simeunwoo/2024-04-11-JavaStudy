404e1bc0e190562d145063088e3efc185248614d
sist115_1 <sist115_1@sist115>
2024-05-14 09:10:40 +0900

    0514

---
+++ Chapter06_상속_1/src/com/sist/main/MainClass_1.java
@@ -0,0 +1,40 @@
+package com.sist.main;
+/*
+ * 	5장
+ * 	118page => 클래스 구조
+ * 	122page => 객체 생성
+ * 	123page => 객체 사용
+ * 	127page => 접근지정어
+ * 	132page => 패키지
+ * 	136page => import
+ * 	140page => 메소드
+ * 	152page => getter/setter
+ * 	166page => 생성자
+ * 
+ * 	6장
+ * 	194page => 상속 (웹에서는 사용 빈도가 거의 없다)
+ * 	201page => 단일 상속, 오버라이딩
+ * 	207page => 상속 시 접근 제한 => 캡슐화가 깨지는 경향
+ * 	211page => 상속 시 생성자
+ * 	215page => 키워드 (super / this)
+ * 	221page => 객체 형변환
+ * 
+ * 	7장
+ * 	=> 인터페이스
+ * 	
+ * 	8장
+ * 	=> 예외 처리
+ * 		= 직접 처리 => try~catch
+ * 		= 간접 처리 => throws Exception
+ * 	--------------------------------------------- 자바 기본 형식 (문법)
+ * 	9장 ~ : 라이브러리 (필요한 메소드)
+ * 			Math => random() / ceil()
+ */
+public class MainClass_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
