26865951fdcc95f91f705bc5e8a81c5b220660ed
sist115_1 <sist115_1@sist115>
2024-05-16 15:52:46 +0900

    0516

---
+++ Chapter07_추상클래스_2/src/com/sist/music/GenieMusic.java
@@ -0,0 +1,11 @@
+package com.sist.music;
+
+public class GenieMusic extends MusicSystem {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Chapter07_추상클래스_2/src/com/sist/music/MelonMusic.java
@@ -0,0 +1,11 @@
+package com.sist.music;
+
+public class MelonMusic extends MusicSystem {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
