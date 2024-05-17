61b5c9615c442a5c32c6f16b148e6769ea0d5180
sist115_1 <sist115_1@sist115>
2024-05-17 12:51:10 +0900

    0517

--- Chapter07_인터페이스_1/src/com/sist/main_3/인터페이스_1.java
+++ Chapter07_인터페이스_1/src/com/sist/main_3/인터페이스_1.java
@@ -45,7 +45,80 @@ class C implements A,B
 	}
 	
 }
+/*
+ *  상속을 받은 경우
+ *  1. 변수 => 클래스 선언
+ * 	2. 메소드 => 생성자 => 메소드를 덮어 쓴다
+ * 	
+ * 	class A
+ * 	{
+ * 		int a,b;
+ * 		public void aaa(){}
+ * 	}
+ * 	A aa=new A();
+ * 	변수 = 메소드 ~~~> aa.a / aa.b / aa.aaa()
+ * 
+ * 	class B
+ * 	{
+ * 		int c;
+ * 		public void bbb(){}
+ * 	}
+ * 	B bb=new B();
+ * 	bb.c => bb.bbb()
+ * 
+ * 	class C extends A
+ * 	{
+ * 		// int a,b; public void aaa(){} : 상속 받음
+ * 		int d;
+ * 		public void ddd(){}
+ * 	}
+ * 	C cc=new C(); ~~~> C가 가지고 있는 모든 내용 호출 : a,b,aaa(),d,ddd()
+ * 	A a=new C(); => A 안에 있는 a,b / aaa()는 C가 가지고 있는 aaa()를 사용한다 (ddd()는 사용을 못한다)
+ * 
+ * 	interface A
+ * 	{
+ * 		void aaa();
+ * 	}
+ * 	interface B
+ * 	{
+ * 		void bbb();
+ * 	}						// extends, implements => 상속 과정
+ * 	class C implements A,B
+ * 	{
+ * 		aaa()
+ * 		bbb()
+ * 	}
+ * 	A a=new C() => a.aaa()
+ * 	B b=new C() => b.bbb()
+ * 	C c=new C() => c.aaa(), c.bbb() => 다중 상속 시
+ * 
+ * 	A a; => 제어가 가능 => a가 가지고 있는 변수/메소드만
+ *  B b; => 제어가 가능 => b가 가지고 있는 변수/메소드만
+ */
+interface AA
+{
+	void aaa();
+}
+interface BB extends AA
+{
+	void bbb();
+}
+class CC implements BB
+{
 
+	@Override
+	public void bbb() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aaa() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
 public class 인터페이스_1 {
 
 	public static void main(String[] args) {
