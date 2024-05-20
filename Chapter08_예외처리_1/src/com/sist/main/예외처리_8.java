260a74931861b106a7b3789f47d158e364ba2bd9
sist115_1 <sist115_1@sist115>
2024-05-20 15:50:16 +0900

    0520

--- Chapter08_예외처리_1/src/com/sist/main/예외처리_8.java
+++ Chapter08_예외처리_1/src/com/sist/main/예외처리_8.java
@@ -18,12 +18,101 @@ package com.sist.main;
  * 		파일 닫기 / 서버 닫기 / 오라클 닫기 => 제대로 안 닫으면 => DDOS 공격 위험 !!!
  * 		=> try 수행, catch 수행 => 무조건 수행이 된다
  * 	}
+ * 
+ * 	흐름
+ * 
+ * 	# 에러가 없는 상태
+ * 
+ * 	문장 1
+ * 	문장 2
+ * 	try
+ * 	{
+ * 		문장 3
+ * 		문장 4
+ * 		문장 5
+ * 	}catch(예외 처리 클래스)
+ * 	{
+ * 		문장 6
+ * 	}
+ * 	finally
+ * 	{
+ * 		문장 7
+ * 	}
+ * 
+ * 	순서 : 1-2-3-4-5-7
+ * 
+ * 	# 에러가 있는 상태
+ * 
+ * 	문장 1
+ * 	문장 2 => 에러 1 발생 => 종료
+ * 	try
+ * 	{
+ * 		문장 3
+ * 		문장 4 => 에러 2 발생 => catch 이동 후에 finally로 이동 (finally은 어떤 경우에도 무조건 수행)
+ * 		문장 5
+ * 	}catch(예외 처리 클래스)
+ * 	{
+ * 		문장 6
+ * 	}
+ * 	finally => 필요 시에만 사용
+ * 	{
+ * 		문장 7 => 에러와 상관 없이 무조건 수행
+ * 	}
+ * 
+ * 	에러 1 발생 시 순서 : 1-2
+ * 	에러 2 발생 시 순서 : 1-2-3-6-7
  */
 public class 예외처리_8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// 에러가 없는 상태
+		System.out.println("1");
+		System.out.println("2");
+		try
+		{
+			System.out.println("3");
+			System.out.println("4");
+			System.out.println("5");
+		}catch(Exception e)
+		{
+			System.out.println("에러 처리 => 6");
+		}
+		finally
+		{
+			System.out.println("7");
+		}
+/*
+1
+2
+3
+4
+5
+7
+ */
+		// 에러가 있는 상태
+		System.out.println("1");
+		System.out.println("2");
+		try
+		{
+			System.out.println("3");
+			System.out.println(10/0); // catch로 이동
+			System.out.println("5");
+		}catch(Exception e)
+		{
+			System.out.println("에러 처리 => 6"); // 에러 확인 => printStackTrace(), getMessage()
+		}
+		finally
+		{
+			System.out.println("7"); // 파일 닫기, 오라클 닫기 => 서버 부하가 심하다 => 닫기
+		}
+/*
+1
+2
+3
+에러 처리 => 6
+7
+ */
 	}
 
 }
