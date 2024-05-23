56589a20c62416fe3add8d98532d3e95b711388f
sist115_1 <sist115_1@sist115>
2024-05-23 09:16:40 +0900

    0523

---
+++ Chapter10_라이브러리(java.util)_1/src/com/sist/util/라이브러리_1.java
@@ -0,0 +1,53 @@
+package com.sist.util;
+/*
+ * 	라이브러리 : 자바에서 지원하는 클래스의 집합
+ *                        ---------- 관련된 클래스 모음 (패키지)
+ *	1. java.lang
+ *
+ *		# Object : 최상위 클래스 => 모든 클래스 (사용자 정의)의 상속을 내리는 클래스
+ *			=> 기능(메소드)
+ *			toString() : 객체를 문자열화
+ *			clone() : 복제 (새로운 메모리를 생성) => prototype
+ *			equals() : 객체 비교
+ *			finalize() : 소멸자
+ *			hashCode() : 객체 주소를 반환
+ *			=> Object는 가장 큰 데이터형
+ *
+ *		# String : 문자열을 저장하는 데이터형
+ *			=> 웹에서 가장 많이 사용되는 데이터형
+ *			=> 기능(메소드)
+ *			length() : 문자 개수 확인 => int
+ *			equals() : 실제 저장된 문자열 비교 => 대소문자 구분
+ *			substring() : 문자열을 자르는 경우 => 화면 UI => CSS ...
+ *			trim() : 좌우의 공백 제거 => 사용자 입력에서 실수 ...
+ *			valueOf() : 모든 데이터형을 문자열로 변환
+ *			replace(), replaceAll() : 문자 변환
+ *			indexOf(), lastIndexOf() : 문자, 문자열의 위치 찾기
+ *
+ *		# StringBuffer : 임시 저장 장소를 만들어서 문자열 결합 => 최적화
+ *			append() : 문자열 결합
+ *
+ *		# Math : 수학 연산
+ *			random() : 임의 추출
+ *			ceil() : 올림
+ *
+ *		# System : 운영 체제와 연결
+ *			print, printf, println
+ *			gc() : 메모리 회수
+ *			exit() : 프로그램 종료
+ *
+ *		# Wrapper : 기본 데이터형 (int, long, double)을 쉽게 사용이 가능하게 기능 부여
+ *			int => Integer =====> Integer.parseInt()
+ *			double => Double =====> Double.parseDouble()
+ *			boolean => Boolean =====> Boolean.parseBoolean()
+ *			byte => Byte
+ *			-------------------> 변환 (문자열) => 정수, 실수, 논리형
+ */
+public class 라이브러리_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
