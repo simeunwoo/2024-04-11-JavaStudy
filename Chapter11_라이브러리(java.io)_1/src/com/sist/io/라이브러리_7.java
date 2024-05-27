2382383e3a296aea80418182af153d8c563c17de
sist115_1 <sist115_1@sist115>
2024-05-27 15:07:45 +0900

    0527

---
+++ Chapter11_라이브러리(java.io)_1/src/com/sist/io/라이브러리_7.java
@@ -0,0 +1,65 @@
+package com.sist.io;
+import java.util.*;
+import java.io.*;
+class Sawon
+{
+	private int sabun; // 사번
+	private String name; // 이름
+	private String dept; // 부서명
+	private String job; // 직위
+	private String loc; // 근무지
+	private int pay; // 연봉
+	public int getSabun() {
+		return sabun;
+	}
+	public void setSabun(int sabun) {
+		this.sabun = sabun;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	public int getPay() {
+		return pay;
+	}
+	public void setPay(int pay) {
+		this.pay = pay;
+	}
+	
+}
+
+public class 라이브러리_7 {
+	public void fileInput()
+	{
+		
+	}
+	public void fileOutput()
+	{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
