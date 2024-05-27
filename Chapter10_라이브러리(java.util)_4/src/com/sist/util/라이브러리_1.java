7d5c92e1cbf948402f1432c3e3e3198a8266e140
sist115_1 <sist115_1@sist115>
2024-05-27 09:14:20 +0900

    0527

---
+++ Chapter10_라이브러리(java.util)_4/src/com/sist/util/라이브러리_1.java
@@ -0,0 +1,16 @@
+package com.sist.util;
+/*
+ * 	java.util : 가장 많이 사용되는 라이브러리
+ * 	=> 난수 : Random => 정수, 지정된 범위
+ * 			=> 1. 객체 생성 : Random r=new Random()
+ * 			=> 2. 난수 발생 : r.nextInt(100) => 100은 제외 (0~99)
+ * 			=> 웹 : 예약일 / 예약 시간 => 난수 활용
+ */
+public class 라이브러리_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
