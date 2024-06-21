4b7127afcf5f17a2961fcd52a6ea8737d29862a3
sist115_1 <sist115_1@sist115>
2024-06-21 09:17:45 +0900

    0621

--- Project_0차_3조/src/com/sist/client/ChatPanel.java
+++ Project_0차_3조/src/com/sist/client/ChatPanel.java
@@ -1,82 +1,146 @@
 package com.sist.client;
-import javax.swing.*;
-
-import com.sist.dao.BoardDAO;
-import com.sist.dao.GoodsDAO;
+import java.util.*;
 import java.awt.*;
-import java.util.ArrayList;
-public class BoardUpdatePanel extends JPanel{
-
-    JLabel titleLa,subLa,contentLa,recomLa,pwdLa;
-    JTextField subtf;
-    JPasswordField pwdpf;
-    JTextArea ta;
-    JComboBox<String> box;
+import javax.swing.*;
+import javax.swing.table.*;
+import javax.swing.text.Document;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+public class ChatPanel extends JPanel{
+    JTextPane pane;
+    JTextField tf;
+    JComboBox<String> box1,box2;
+    JTable table; 
+    DefaultTableModel model;
     JButton b1,b2;
-    BoardDAO dao;
-    GoodsDAO gdao;
-    /*
-    타이틀
-    제목
-    내용
-    사원 추천하기
-     */
-    public BoardUpdatePanel()
+    
+    JLabel la;
+    JTextField youTf, sendTf;
+    JTextArea ta;
+    JButton ob;
+    JPanel pan = new JPanel();
+    public ChatPanel()
     {
-    	dao=BoardDAO.newInstance();
-    	gdao=GoodsDAO.newInstance();
-    	
     	setLayout(null);
-    	titleLa=new JLabel("제품 후기",JLabel.CENTER);
-    	titleLa.setFont(new Font("휴먼모음T",Font.BOLD,35));
-    	titleLa.setBounds(175, 50, 930, 40);
-    	add(titleLa);
+    	pane=new JTextPane();
+    	JScrollPane js1=new JScrollPane(pane);
+    	js1.setBounds(10, 15, 480, 480);
+    	add(js1);
+    	pane.setEditable(false);
+    	pane.setBackground(Color.LIGHT_GRAY);
     	
-    	subLa=new JLabel("제목",JLabel.RIGHT);
-    	subLa.setBounds(250, 165, 90, 30);
-    	add(subLa);
+    	tf=new JTextField();
+    	tf.setBounds(10, 500, 380, 30);
+    	add(tf);
     	
-    	subtf=new JTextField();
-    	subtf.setBounds(365, 165, 550, 30);
-    	add(subtf);
+    	box1=new JComboBox<String>();
+    	box1.addItem("black");
+    	box1.addItem("cyan");
+    	box1.addItem("yellow");
+    	box1.addItem("blue");
+    	box1.addItem("magenta");
+    	box1.addItem("green");
+    	box1.addItem("pink");
+    	box1.addItem("orange");
     	
-    	recomLa=new JLabel("제품",JLabel.RIGHT);
-    	recomLa.setBounds(250, 120, 90, 30);
-    	add(recomLa);
+    	box1.setBounds(395, 500, 100, 30);
+    	add(box1);
     	
+    	String[] col={"ID","이름","성별"};
+    	String[][] row=new String[0][3];
     	
-    	contentLa=new JLabel("내용",JLabel.RIGHT);
-    	contentLa.setBounds(250, 210, 90, 30);
-    	add(contentLa);
-    	
-    	ta=new JTextArea();
-    	JScrollPane js=new JScrollPane(ta);
-    	js.setBounds(365, 210, 550, 250);
-    	add(js);
-    	
-    	pwdLa=new JLabel("비밀 번호",JLabel.CENTER);
-    	pwdpf=new JPasswordField();
-    	pwdLa.setBounds(250, 640, 80, 30);
-    	pwdpf.setBounds(365, 640, 150, 30);
-    	add(pwdLa);add(pwdpf);
-    	
-    	b1=new JButton("수정 완료");
-    	b1.setBounds(765, 500, 150, 30);
-    	add(b1);
-    	
-    	b2=new JButton("취소");
-    	b2.setBounds(920, 500, 150, 30);
-    	add(b2);
-    	
+    	model=new DefaultTableModel(row,col)
+    	{
 
-    }
-    public void printBox(String id) {
-    	gdao=GoodsDAO.newInstance();
-    	ArrayList<String> list_temp = gdao.buyGoodsName(id);
-    	String[] list = list_temp.toArray(new String[list_temp.size()]);
-    	box=new JComboBox(list);
-    	box.setBounds(365, 120, 550, 30);
-    	add(box);
-    }
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+    		
+    	};
+    	table=new JTable(model);
+    	JScrollPane js2=new JScrollPane(table);
+    	js2.setBounds(495, 15, 400, 200);
+    	add(js2);
+    	
+    	box2=new JComboBox<String>();
+    	box2.setBounds(495,220, 100, 30);
+    	add(box2);
+    	
+    	b1=new JButton("1:1상담");
+    	b1.setBounds(600, 220, 120, 30);
+    	
+    	b2=new JButton("정보보기");
+    	b2.setBounds(725, 220, 100, 30);
+    	
+    	add(b1);add(b2);
+    	
+    	la = new JLabel("1 : 1");
+    	
+    	youTf = new JTextField(10);
+    	youTf.setEnabled(false);
+    	sendTf = new JTextField(30);
+    	ob = new JButton("종료");
 
+    	ta = new JTextArea();
+    	JScrollPane js3 = new JScrollPane(ta);
+    	ta.setEditable(false);
+    	
+    	
+    	
+    	pan.setLayout(new BorderLayout());
+    	
+    	JPanel p = new JPanel();
+    	p.add(la); p.add(youTf); p.add(ob);
+    	pan.add("North", p);
+    	
+    	pan.add("Center", js3);
+    	pan.add("South", sendTf);
+    	
+    	pan.setBounds(495, 250, 400, 250);
+    	add(pan);
+    	
+    	pan.setVisible(false);
+    	
+    }
+    public void initStyle()
+    {
+ 	   Style green=pane.addStyle("green", null);
+ 	   StyleConstants.setForeground(green, Color.green);
+ 	   
+ 	   Style yellow=pane.addStyle("yellow", null);
+ 	   StyleConstants.setForeground(yellow, Color.yellow);
+ 	   
+ 	   Style blue=pane.addStyle("blue", null);
+ 	   StyleConstants.setForeground(blue, Color.blue);
+ 	   
+ 	   Style pink=pane.addStyle("pink", null);
+ 	   StyleConstants.setForeground(pink, Color.pink);
+ 	   
+ 	   Style cyan=pane.addStyle("cyan", null);
+ 	   StyleConstants.setForeground(cyan, Color.cyan);
+ 	   
+ 	   Style orange=pane.addStyle("orange", null);
+ 	   StyleConstants.setForeground(orange, Color.orange);
+ 	   
+ 	   Style magenta=pane.addStyle("magenta", null);
+ 	   StyleConstants.setForeground(magenta, Color.magenta);
+        
+ 	   Style red=pane.addStyle("red", null);
+ 	   StyleConstants.setForeground(red, Color.red);
+ 	   
+    }
+    
+    // TextPane의 단점 => 문자열 결합 => setText()
+    public void append(String msg,String color)
+    {
+    	try
+    	{
+    	  Document doc=pane.getDocument();
+    	  doc.insertString(doc.getLength(), msg+"\n", pane.getStyle(color));
+    	}catch(Exception ex){}
+    	
+    }
+    
 }
--- Project_0차_3조/src/com/sist/client/GoodsDetailPanel.java
+++ Project_0차_3조/src/com/sist/client/GoodsDetailPanel.java
@@ -1,67 +1,143 @@
 package com.sist.client;
 
-import com.sist.dao.*;
-import javax.swing.*;
-import javax.swing.table.*;
-import java.awt.*;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
-public class FindPanel extends JPanel{
-	JTable table;
-	DefaultTableModel model;
-	JTextField tf;
-	JButton b;
-	EmpMemberDAO dao;
-	TableColumn column;
-	String[] option = {"부서명", "사원명", "직위"};
-	JComboBox<String> cb;
-	
-	public FindPanel() {
-		dao = EmpMemberDAO.newInstance();
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.sist.common.ImageChange;
+import com.sist.dao.GoodsDAO;
+import com.sist.dao.GoodsVO;
+import com.sist.dao.MemberDAO;
+
+public class GoodsDetailPanel extends JPanel implements ItemListener{
+	public JLabel la1, la2, la3, la4, la5, la6, la7, la8, laId;
+	public JButton b1, b2;
+	public JComboBox<Integer> box;
+	public JComboBox<String> boxEmp;
+	JLabel pLa, tLa;
+	ControlPanel cp;
+	int no = 0;
+	GoodsDAO dao;
+	MemberDAO dao2;
+	public GoodsDetailPanel(ControlPanel cp) {
+		this.cp = cp;
+		dao = GoodsDAO.newInstance();
+		dao2 = MemberDAO.newInstance();
+		String[] list = dao2.getEmp();
+		setLayout(null);
+		la1 = new JLabel();
+		la1.setBounds(10, 15, 450, 550);
+		add(la1);
 		
-		setLayout(null); // 위아래 배치(North,South) // 똑같은거여러개 => Grid // 내가 지정하기 null
-		tf = new JTextField(20);
-		b = new JButton("검색");
-		cb = new JComboBox<String>(option);
+		la2 = new JLabel();
+		la2.setBounds(470, 15, 400, 45);
+		add(la2);
 		
-		JPanel p =new JPanel();
-		p.add(cb);
-		p.add(tf);
-		p.add(b);
-		p.setBounds(390, 20, 500, 30);
+		laId = new JLabel();
+		
+		la3 = new JLabel();
+		la3.setBounds(470, 70, 350, 30);
+		add(la3);
+		
+		la4 = new JLabel();
+		la4.setBounds(470, 110, 60, 30);
+		add(la4);
+		
+		la5 = new JLabel();
+		la5.setBounds(540, 110, 120, 30);
+		add(la5);
+		
+		la6 = new JLabel();
+		la6.setBounds(470, 150, 120, 30);
+		add(la6);
+
+		la7 = new JLabel();
+		la7.setBounds(600, 150, 120, 30);
+		add(la7);
+		
+		la8 = new JLabel();
+		la8.setBounds(470, 190, 120, 30);
+		add(la8);
+		
+
+		b1 = new JButton("장바구니");
+		b2 = new JButton("목록");
+		JPanel p = new JPanel();
+		
+		p.add(b1); p.add(b2);
+		p.setBounds(470, 400, 350, 35);
 		add(p);
 		
-		String[] col = {"사번", "이름", "직위", "연봉", "입사일", "부서명", "근무지", "실적"};
-		Object[][] row = new Object[0][8];
+		pLa = new JLabel("금액");
+		pLa.setBounds(470, 270, 200, 30);
+		add(pLa);
 		
-		model = new DefaultTableModel(row,col) {
-			@Override
-			public boolean isCellEditable(int row, int column) {
-				return false;
-			}
-		};
+		tLa = new JLabel("구매 총 금액");
+		tLa.setBounds(470, 310, 200, 30);
+		add(tLa);
 		
-		table = new JTable(model);
-		table.setRowHeight(35);
-		table.getTableHeader().setReorderingAllowed(false);
-		table.setShowVerticalLines(false); // Vertical
-		JScrollPane js = new JScrollPane(table);
-		js.setBounds(70, 60, 1100, 500);
-		add(js);
+		box = new JComboBox<Integer>();
+		box.addItem(1);
+		box.addItem(2);
+		box.addItem(3);
+		box.addItem(4);
+		box.addItem(5);
+		box.addItem(6);
+		box.addItem(7);
 		
-		for (int i = 0; i < col.length; i++) {
-			column = table.getColumnModel().getColumn(i);
-			if(i==0) 
-				column.setPreferredWidth(30);
-			else if(i==1)
-				column.setPreferredWidth(100);
-			else if(i==2)
-				column.setPreferredWidth(500);
-			else if(i==3)
-				column.setPreferredWidth(150);
+		box.setBounds(470, 230, 200, 30);
+		add(box);
+		
+		boxEmp = new JComboBox<String>(list);
+		boxEmp.setBounds(470, 350, 100, 35);
+		add(boxEmp);
+		
+		box.addItemListener(this);
+	}
+	public void print(int no) {
+		laId.setText(String.valueOf(no));
+		GoodsVO vo = dao.goodsDetailData(no);
+		System.out.println(vo.getGoods_name());
+		try {
+			URL url = new URL(vo.getGoods_poster());
+			Image img = ImageChange.getImage(new ImageIcon(url), 450, 550);
+			la1.setIcon(new ImageIcon(img));
+		} catch (Exception e) {}
+		la2.setText("<html><fong size=12><b>" + vo.getGoods_name() + "</b></font></html>");
+		la3.setText("<html><font color = gray>" + vo.getGoods_sub() + "</font></html");
+		la4.setText("<html><font color = magenta>" + vo.getGoods_discount() + "</font></html>");
+		la5.setText(vo.getGoods_price());
+		la6.setText("<html><font color = green></font></html>");
+		la6.setText("<html><font color = green>" + vo.getGoods_first_price() + "</font></html>");
+		la7.setText("배송 : " + vo.getGoods_delivery());
+		pLa.setText("가격 : " +vo.getGoods_price());
+		tLa.setText("구매 총 가격 : " + vo.getGoods_price());
+	}
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		if(e.getSource() == box){
+			String price = pLa.getText();
+			price = price.substring(price.indexOf(":") + 1);
+			price = price.replaceAll("[^0-9]", "");
+			
+			int account = box.getSelectedIndex() + 1;
+			int total = Integer.parseInt(price) * account;
+			
+			DecimalFormat df = new DecimalFormat("##,###,###");
+			String s = df.format(total);
+			tLa.setText("구매 총 가격 : " + total + "원");
 		}
 		
-		
-		
 	}
-
 }
